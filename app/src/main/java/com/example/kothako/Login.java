93181f76c2a158e679ed4d626b07d5eb8951bc68
Aleef <abdullahmdkhalid2031@gmail.com>
2024-08-01 20:13:39 +0600

    dekh login .xml

--- app/src/main/java/com/example/kothako/Login.java
+++ app/src/main/java/com/example/kothako/Login.java
@@ -1,6 +1,5 @@
 package com.example.kothako;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,28 +8,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.google.firebase.auth.FirebaseAuth;
-
-public class MainActivity extends AppCompatActivity {
-    FirebaseAuth auth;
-
-
-
-
-
-
-
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        auth=FirebaseAuth.getInstance();
-        if (auth.getCurrentUser() == null) {
-            Intent intent=new Intent(MainActivity.this,Login.class);
-            startActivity(intent);
-        }
+        setContentView(R.layout.activity_login);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
