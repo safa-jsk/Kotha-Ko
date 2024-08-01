abdaa8684701319c6bf597a841ce7250a58f32fc
Saadat <md.saadat.rahman@g.bracu.ac.bd>
2024-08-01 16:42:39 +0600

    Splash Screen done and Firebase Connected.

--- app/src/main/java/com/example/kothako/splash.java
+++ app/src/main/java/com/example/kothako/splash.java
@@ -1,6 +1,8 @@
 package com.example.kothako;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,9 +14,20 @@ public class splash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_splash);
+            super.onCreate(savedInstanceState);
+            EdgeToEdge.enable(this);
+            setContentView(R.layout.activity_splash);
+//            getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },4000);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
