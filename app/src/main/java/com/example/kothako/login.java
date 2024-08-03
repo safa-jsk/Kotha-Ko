e1b5cd0f858ecf374f3f42e1a2d46a5156f89fe7
Jabir Safa Khandoker <jabirkhandoker03@gmail.com>
2024-08-03 14:41:30 +0600

    Configured the Login class

--- app/src/main/java/com/example/kothako/login.java
+++ app/src/main/java/com/example/kothako/login.java
@@ -3,14 +3,29 @@ package com.example.kothako;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class login extends AppCompatActivity {
+    Button button;
+    EditText email, pass;
+    FirebaseAuth auth;
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +37,47 @@ public class login extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        auth = FirebaseAuth.getInstance();
+        button = findViewById(R.id.button_login);
+        email = findViewById(R.id.edit_email);
+        pass = findViewById(R.id.edit_pass);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Email = email.getText().toString();
+                String Pass = pass.getText().toString();
+
+                if (TextUtils.isEmpty(Email)){
+                    Toast.makeText(login.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                } else if (TextUtils.isEmpty(Pass)){
+                    Toast.makeText(login.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                } else if (!Email.matches(emailPattern)){
+                    email.setError("Give Proper Gmail Address");
+                } else if (pass.length() < 6 || pass.length() > 20){
+                    pass.setError("Password must be between 6 - 20 characters");
+                    Toast.makeText(login.this, "Password must be between 6 - 20 characters", Toast.LENGTH_SHORT).show();
+                } else {
+                    auth.signInWithEmailAndPassword(Email, Pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()){
+                                try{
+                                    Intent intent = new Intent(login.this, MainActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                } catch (Exception e){
+                                    Toast.makeText(login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(login.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+                }
+            }
         });
     }
 }
